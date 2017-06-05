dd4d916425ed9777427b49082b06ea84c8c1241b
diegoandres <diego.andrs@gmail.com>
2017-06-05 23:42:17 +0000

    autentificacion

--- dwfinal/users/urls.py
+++ dwfinal/users/urls.py
@@ -4,5 +4,6 @@ from users import views
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^$', views.listar, name='usuarios'),
     # url(r'^$', include('perfil.urls')),
 ]
